1979e891c1e3db0927130a7fbf59539991b7fdb8
danybri <dany_cdbv@hotmail.com>
2015-06-23 21:32:50 -0500

    999 Counter
    0 to 999 counter

--- main.c
+++ main.c
@@ -19,7 +19,21 @@
 #define setPortD(x) 	(GPIOD_PDOR |= (1 << x))
 #define clearPortD(x) 	(GPIOD_PDOR &= ~(1 << x))
 
-#define readPortB(x)	((GPIOB_PDIR &= (1 << x)) >> x)
+//LCD Signals
+#define portLCD			GPIOC_PDOR
+#define output_RS(x)	 	if(x){GPIOB_PDOR |= (0x00000001);}else{GPIOB_PDOR &= (0xFFFFFFFE);}
+#define output_Enable(x) 	if(x){GPIOB_PDOR |= (0x00000002);}else{GPIOB_PDOR &= (0xFFFFFFFD);}
+
+#define high_RS			(GPIOB_PDOR |= (0x00000001))	
+#define	low_RS			(GPIOB_PDOR &= (0xFFFFFFFE))
+#define high_Enable		(GPIOB_PDOR |= (0x00000002))	
+#define	low_Enable		(GPIOB_PDOR &= (0xFFFFFFFD))
+
+#define	nIns			0
+#define	nData			1
+
+
+#define readPortB(x)	((GPIOD_PDIR &= (1 << x)) >> x)
 #define	nButtonPressed  0
 #define	nButtonFree     1 
 
@@ -36,45 +50,80 @@
 //Time
 #define	n1sec			2000000  
 #define	n2sec			4000000
-#define	n500ms			1000000  
+#define	n500msed		1000000 
+#define n15msec			35000
+#define n40usec			3500 
 //--------------------------------------------------------------
 //Declare Prototypes
 void cfgPorts(void);
 void delay(long ltime);
-             
-int main(void)
-{	
-	cfgPorts();	
-	
-	turnOffGreenLed;
-	turnOffRedLed;
-	turnOffBlueLed;
-	
-	for(;;)
-	{
-	if(readPortB(btn1) == 1)
-	{
+void initLCD(void);
+void sendCode(int Code, int Data);
+void ASCII(unsigned char Number);
+
+unsigned char LCD_write(unsigned char Code, unsigned char *Data, unsigned Size);
+
+   
+//Cursor Blink off initialization
+const unsigned char InitializeLCD[5] = {0x38, 0x38, 0x38, 0x0C, 0x01};
+unsigned char Name[] = {0x80, 0x67};
+unsigned char Position[]={0x86};
+//Contador
+static unsigned char nUnidad;
+static unsigned char nDecena;
+static unsigned char nCentena;
+
+
+int main(void){	
+	cfgPorts();
+	initLCD();
+	nUnidad=0;
+	nDecena=0;
+	nCentena=0;
+
+
 	for(;;)
     {
-		//if((GPIOB_PDIR & 0x00000008) == 0)
+
+		
+		if(nUnidad<=9)
+		{
+			nUnidad++;	
+			delay(n1sec);
+		}
+		
+		if(nUnidad>9)
+		{
+			if(nDecena <= 9)
+			{
+				nDecena++;
+				nUnidad=0;
+			}
 			
-		if(readPortB(btn1) == 0)
-		{
-			turnOnGreenLed;
-			turnOffRedLed;
-			turnOffBlueLed;
+			if(nDecena > 9 )
+			{
+				if(nCentena <= 9)
+				{
+					nCentena++;
+					nDecena=0;
+					nUnidad=0;
+				}
+
+				if(nCentena > 9)
+				{
+					nDecena=0;
+					nUnidad=0;
+					nCentena=0;
+				}
+			}									
 		}
 		
-		else
-		{
-			turnOnRedLed;
-			turnOffGreenLed;
-			turnOffBlueLed;
-		}
-    }
-		
-	}	
-		
+		sendCode(nIns, 0x88);
+		ASCII(nUnidad);
+		sendCode(nIns, 0x87);
+		ASCII(nDecena);
+		sendCode(nIns, 0x86);
+		ASCII(nCentena);
     } 
     return 0;
 }
@@ -83,18 +132,135 @@ void cfgPorts(void)
 {
     //Turn on clock for portb and portd
     SIM_SCGC5 = SIM_SCGC5_PORTB_MASK;
-    SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK;   
+    SIM_SCGC5 |= SIM_SCGC5_PORTC_MASK;
+	SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK;   
      
     /* Set pins of PORTB as GPIO */
-    PORTB_PCR3 	= PORT_PCR_MUX(1);
+    //Connected to LCD
+	PORTC_PCR0 	= PORT_PCR_MUX(1);
+	PORTC_PCR1 	= PORT_PCR_MUX(1);
+	PORTC_PCR2 	= PORT_PCR_MUX(1);
+	PORTC_PCR3 	= PORT_PCR_MUX(1);
+	PORTC_PCR4 	= PORT_PCR_MUX(1);
+	PORTC_PCR5 	= PORT_PCR_MUX(1);
+	PORTC_PCR6 	= PORT_PCR_MUX(1);
+	PORTC_PCR7 	= PORT_PCR_MUX(1);
+	
+	//LCD: RS and Enable
+	PORTB_PCR0 	= PORT_PCR_MUX(1);
+	PORTB_PCR1 	= PORT_PCR_MUX(1);
+	
+	//Leds on board
 	PORTB_PCR18 = PORT_PCR_MUX(1);
     PORTB_PCR19 = PORT_PCR_MUX(1);
 	PORTD_PCR1 	= PORT_PCR_MUX(1);
      
     //Configure all PortB as outputs
-    GPIOB_PDDR = 0xFFFFFFF7; //1111 1111 1111 1111 1111 1111 1111 0111
+    GPIOB_PDDR = 0xFFFFFFFF; //1111 1111 1111 1111 1111 1111 1111 0111
      
     //Configure all PortD as outputs
+    GPIOC_PDDR = 0xFFFFFFFF;
+	
+	//Configure all PortD as outputs
     GPIOD_PDDR = 0xFFFFFFFF;
 }
 //--------------------------------------------------------------
+void delay(long ltime)
+{
+	while (ltime > 0)
+	{
+		ltime--;
+	}
+}
+//--------------------------------------------------------------
+void initLCD(void)
+{
+	int i;
+	delay(n15msec);
+	
+	/* Send initialization instructions */
+	/* Loop for sending each character from the array */
+	for(i=0;i<5;i++)
+	{										
+		sendCode(nIns, InitializeLCD[i]);	/* send initialization instructions */			
+	}
+}
+//--------------------------------------------------------------
+void sendCode(int Code, int Data)
+{
+	//Assign a value to pin RS
+	/*HINT: When RS is 1, then the LCD receives a data
+	when RS is 0, then the LCD receives an instruction */
+	// Initialize RS and Enable with 0
+	low_RS;
+	low_Enable;
+	
+	//Assign the value we want to send to the LCD
+	portLCD = Data;	
+	
+	//We make the algorithm to establish if its an instruction we start with 0 on RS value, otherwise if its a data command we start with RS as 1;
+	if (Code == nIns)
+	{
+		low_RS;
+		high_Enable;
+		delay(n40usec);
+		low_Enable;
+		low_RS;
+	}		
+	else if (Code == nData)
+	{
+		high_RS;
+		high_Enable;
+		delay(n40usec);
+		low_Enable;
+		low_RS;
+	}
+}
+
+//--------------------------------------------------------------
+unsigned char LCD_write(unsigned char Code, unsigned char *Data, unsigned Size)
+{
+	while(Size != 0)
+	{
+		/*HINT: When RS is 1, then the LCD receives a data
+		when RS is 0, then the LCD receives an instruction */
+		// Initialize RS and Enable with 0
+		low_RS;
+		low_Enable;
+		
+		//Assign the value we want to send to the LCD
+		portLCD = *Data;	
+		
+		//We make the algorithm to establish if its an instruction we start with 0 on RS value, otherwise if its a data command we start with RS as 1;
+		if (Code == nIns)
+		{
+			low_RS;
+			high_Enable;
+			delay(n40usec);
+			low_Enable;
+			low_RS;
+		}		
+		else if (Code == nData)
+		{
+			high_RS;
+			high_Enable;
+			delay(n40usec);
+			low_Enable;
+			low_RS;
+		}
+		
+		Size --;
+		Data++;
+	}
+	
+	return 0;
+}
+
+
+void ASCII(unsigned char Number)
+{
+	Number = Number + 0x30;
+	
+	sendCode(nData,Number);
+	
+}
